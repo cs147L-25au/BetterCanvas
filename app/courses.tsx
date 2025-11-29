2e9192389d6481aa9a7c4ca2fe8197efd8b02aa7
Hannah Cha <150081324+hannahcha417@users.noreply.github.com>
2025-11-29 15:28:50 -0800

    Add course tab (#5)
    * Add screen/tab nav for courses tab
    
    * Rename AgendaScreen to CoursesScreen

---
+++ app/courses.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+import { Screen } from "@/components/Screen";
+
+export default function CoursesScreen() {
+  return (
+    <Screen>
+      <Text>My Courses Screen</Text>
+    </Screen>
+  );
+}
