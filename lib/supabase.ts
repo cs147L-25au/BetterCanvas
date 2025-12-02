20eab7cffb2246d455191b8b3fa055d47726dde8
hannahcha417 <hcha417@stanford.edu>
2025-12-01 16:26:01 -0800

    Add error throw instead of non-null assertion

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,14 @@
-import 'react-native-url-polyfill/auto';
-import { createClient } from '@supabase/supabase-js';
-import { Database } from '../types/database';
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../types/database";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    "Missing Supabase environment variables: please set EXPO_PUBLIC_SUPABASE_URL and EXPO_PUBLIC_SUPABASE_ANON_KEY in an .env file at the project base!"
+  );
+}
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
