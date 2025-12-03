299b0842cc6e329bc094666b5f5d61bb3c074d3f
Hannah Cha <150081324+hannahcha417@users.noreply.github.com>
2025-12-02 21:41:01 -0800

    supabase set up (#6)
    * Add screen/tab nav for courses tab
    
    * Add .env to .gitignore for security
    
    * Add supabase details and types
    
    * Get rid of unneeded exports
    
    * Export Database interface
    
    * Add error throw instead of non-null assertion

---
+++ types/database.ts
@@ -0,0 +1,66 @@
+type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      courses: {
+        Row: {
+          id: string;
+          course_number: string;
+          course_name: string;
+          created_at: string;
+        };
+        Insert: {
+          id?: string;
+          course_number: string;
+          course_name: string;
+          created_at?: string;
+        };
+        Update: {
+          id?: string;
+          course_number?: string;
+          course_name?: string;
+          created_at?: string;
+        };
+      };
+      assignments: {
+        Row: {
+          id: string;
+          course_id: string;
+          assignment_name: string;
+          due_date: string;
+          estimated_duration: number;
+          created_at: string;
+        };
+        Insert: {
+          id?: string;
+          course_id: string;
+          assignment_name: string;
+          due_date: string;
+          estimated_duration: number;
+          created_at?: string;
+        };
+        Update: {
+          id?: string;
+          course_id?: string;
+          assignment_name?: string;
+          due_date?: string;
+          estimated_duration?: number;
+          created_at?: string;
+        };
+      };
+    };
+  };
+}
+
+// Helper types
+type Course = Database["public"]["Tables"]["courses"]["Row"];
+type Assignment = Database["public"]["Tables"]["assignments"]["Row"];
+type CourseInsert = Database["public"]["Tables"]["courses"]["Insert"];
+type AssignmentInsert = Database["public"]["Tables"]["assignments"]["Insert"];
