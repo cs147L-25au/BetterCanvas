c95d18c198cc71222422cb83426e8232dae1aa31
Kelly Bonilla Guzman <kellybonillaguzman@gmail.com>
2025-12-05 15:58:48 -0800

    change course + agenda item colors

--- assets/Themes/colors.ts
+++ assets/Themes/colors.ts
@@ -6,3 +6,16 @@ export const colors = {
   textSecondary: "#666",
   textError: "#d32f2f",
 };
+
+export const lightToDarkColorMap: Record<string, string> = {
+  "#FFFFFF": "#373530",
+  "#F1F1EF": "#787774",
+  "#F3EEEE": "#976D57",
+  "#F8ECDF": "#CC782F",
+  "#FAF3DD": "#C29343",
+  "#EEF3ED": "#548164",
+  "#E9F3F7": "#487CA5",
+  "#F6F3F8": "#8A67AB",
+  "#F9F2F5": "#B35488",
+  "#FAECEC": "#C4554D",
+};
